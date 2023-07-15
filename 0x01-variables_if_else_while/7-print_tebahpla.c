c6961256f847fa14ce4214609cbf7928b5fe0d8f
Darknught <hassino66@yahoo.com>
2023-07-14 18:47:37 -0700

    If statements

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabets in reverse folloewd by a new line
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter <= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
