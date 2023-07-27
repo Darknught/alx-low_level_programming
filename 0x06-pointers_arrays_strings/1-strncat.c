029dda0f71d591796eaec04bf40a4df2995816ef
Darknught <hassino66@yahoo.com>
2023-07-27 03:44:51 -0700

    Concatinates two strings with strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,8 +17,8 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
-		dest[1 + j] = src[j];
+		dest[i + j] = src[j];
 	}
-	dest[1 + j] = '\0';
+	dest[i + j] = '\0';
 	return (dest);
 }
