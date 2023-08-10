e4fd6963bfd24beb254e70f61e3e87c282d4ed83
Darknught <hassino66@yahoo.com>
2023-08-10 08:47:07 -0700

    Concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,6 +35,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		result[i] = s1[i];
 	for (j = 0; j < n; j++)
 		result[i++] = s2[j];
-	result[i] = '\0';
+	result[i + j] = '\0';
 	return (result);
 }
