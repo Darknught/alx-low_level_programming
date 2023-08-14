f26aef9cce713c342f2edea2bfcfa033e0718a7a
Darknught <hassino66@yahoo.com>
2023-08-14 09:51:17 -0700

    Prints the name of the file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,40 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-
-void my_file(void);
-
 /**
- * main - Entry point
+ * main - prints the name of the file
  * Return: 0 (success)
  */
 int main(void)
 {
-	my_file();
+	printf("%s\n", __FILE__);
 	return (0);
-}
-
-/**
- * my_file - get the current source file name using __FILE__ macro
- * Return: void
- */
-void my_file(void)
-{
-	const char *file_prot = __FILE__;
-	const char *base_name = strrchr(file_prot, '/');
-	const char *file_to_match = "2-main.c";
-	const char *character = file_prot;
-
-	if (base_name != NULL)
-	{
-		file_prot = base_name + 1;
-	}
-	if (strcmp(file_prot, file_to_match) == 0)
-	{
-		while (*character != '\0')
-		{
-			putchar(*character);
-			character++;
-		}
-		putchar('\n');
-	}
 }
