45b04281d4acd64ffa1816b47c5fa43526142ffa
Darknught <hassino66@yahoo.com>
2023-08-16 14:53:21 -0700

    simple calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument to count
+ * @argv: argument to check
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	op = get_op_func(argv[2]);
+
+	if (!op)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * op_add - add two parameters
+ * @a: first parameter
+ * @b: second parameter
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two parameters
+ * @a: first parameter
+ * @b: second parameter
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two parameters
+ * @a: first parameter
+ * @b: second parameter
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - devides two parameters
+ * @a: first parameter
+ * @b: second parameter
+ * Return: result of the division of a and b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
+}
+
+/**
+ * op_mod - gets the remainder of two parameters
+ * @a: first parameter
+ * @b: second parameter
+ * Return: the remainder of the division a by b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
