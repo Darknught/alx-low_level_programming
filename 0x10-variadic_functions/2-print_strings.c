d766bbc855640b198ed7d83008a15140800a0fd3
Darknught <hassino66@yahoo.com>
2023-08-17 05:45:40 -0700

    prints string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,6 +14,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
+	if (separator == NULL)
+		separator = "";
+
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char *);
