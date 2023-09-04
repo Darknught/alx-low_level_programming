cf0bb7877d34f902ec181252fde742fc4a49bd8f
Darknught <hassino66@yahoo.com>
2023-09-04 10:30:18 -0700

    creates an array of chars and initializes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,9 +11,6 @@ int create_file(const char *filename, char *text_content)
 {
 	int op, wt, len = 0;
 
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wt = write(op, text_content, len);
-
 	if (filename == NULL)
 		return (-1);
 
@@ -23,8 +20,12 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wt = write(op, text_content, len);
+
 	if (op == -1 || wt == -1)
 		return (-1);
+
 	close(op);
 
 	return (1);
