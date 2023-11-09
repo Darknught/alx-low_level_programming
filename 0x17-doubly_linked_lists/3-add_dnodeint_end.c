6e4ec4e0fa8288dab9fffacbc48c24c06daed6a6
Darknught <hassino66@yahoo.com>
2023-11-09 15:39:36 +0000

    adds node at the end of the list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,13 +12,19 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new;
 	dlistint_t *tmp;
 
+	if (head == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
 	new->n = n;
+	new->next = NULL;
 
 	if (*head == NULL)
 	{
 		*head = new;
-		new->next = NULL;
 		new->prev = NULL;
 		return (new);
 	}
@@ -29,6 +35,5 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	tmp->next = new;
 	new->prev = tmp;
-	new->next = NULL;
 	return (new);
 }
