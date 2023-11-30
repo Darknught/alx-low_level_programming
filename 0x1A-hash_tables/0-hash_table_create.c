0d2dd992050361cbf606d0aa18da3d87e571a2c7
Darknught <hassino66@yahoo.com>
2023-11-30 17:33:31 +0000

    code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,6 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL);
 	}
+	new->size = size;
 
 	new->array = malloc(sizeof(hash_node_t *) * size);
 
@@ -26,8 +27,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(new);
 		return (NULL);
 	}
-
-	new->size = size;
 	for (i = 0; i < size; i++)
 	{
 		new->array[i] = NULL;
