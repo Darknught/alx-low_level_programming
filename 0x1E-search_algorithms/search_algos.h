2bbf3338f46e414591349200102f14af12e2b5cf
Darknught <hassino66@yahoo.com>
2024-03-12 12:28:11 +0000

    code

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,10 +5,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <math.h>
 
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 
 
