34527d6562f45d90d3dc5cc0c682115d19880970
Darknught <hassino66@yahoo.com>
2024-03-13 22:07:02 +0000

    code

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -8,26 +8,28 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *prev, *node;
+	skiplist_t *prev, *curr = list;
 
 	if (list == NULL)
 		return (NULL);
 
-	prev = list;
-	node = list->express;
-
-	while (node && node->n < value)
+	while (curr && curr->n < value)
 	{
-		prev = node;
-		node = node->express;
-
-		if (node)
-			printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+		prev = curr;
+		if (curr->express == NULL)
+		{
+			while (curr->next)
+				curr = curr->next;
+			break;
+		}
+		printf("Value checked at index [%lu] = [%d]\n",
+				curr->express->index, curr->express->n);
+		curr = curr->express;
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n",
-			prev->index, node ? node->index : prev->index);
+			prev->index, curr->index);
 
-	while (prev && prev->index <= (node ? node->index : prev->index))
+	while (prev && prev->index < curr->index)
 	{
 		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
 
